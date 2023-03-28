b35a2232991ba216147f7483e6684b5578d8399f
halamhung <hungdeptrai111@gmail.com>
2023-03-28 12:54:22 +0700

    up

---
+++ BigSchool/Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using BigSchool.DTOs;
+using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BigSchool.Controllers
+{
+
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbcontext;
+
+        public AttendancesController()
+        {
+            _dbcontext = new ApplicationDbContext();
+           
+        }
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+            var attendance = new Attendance()
+            {
+                CourseId = courseId,
+                AttendeeId = User.Identity.GetUserId(),
+            };
+            _dbcontext.Attendances.Add(attendance);
+            _dbcontext.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
---
+++ BigSchool/Migrations/202303180751259_AddAttendance.cs
@@ -0,0 +1,20 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAttendance : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.Attendances", "CourseId");
+            AddForeignKey("dbo.Attendances", "CourseId", "dbo.Courses", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Attendances", "CourseId", "dbo.Courses");
+            DropIndex("dbo.Attendances", new[] { "CourseId" });
+        }
+    }
+}
